cb8415a55f93b44cc728e204b67b8da1a1a82bce
ktpq <kartpol47@gmail.com>
2025-10-03 00:06:10 +0700

    frontend - login and register

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { onMount } from 'svelte';
+    import 'aos/dist/aos.css';
+    import AOS from 'aos'
+ 
+    onMount(() =>{
+        AOS.init();
+    })
+
+</script>
+
+<main class="bg-[#FEF5ED] h-screen flex justify-center items-center">
+  <section class="grid grid-cols-2 w-[65vw] max-xl:w-full max-xl:grid-cols-1">
+    <div class="px-25 flex flex-col items-center justify-center max-lg:px-15 max-sm:px-5"  data-aos="fade-up" data-aos-duration="1500">
+      <h1 class="text-5xl font-semibold mb-5 text-center">Login</h1>
+
+      <div class="my-4 w-full relative">
+        <img src="./login/email-green.png" alt="" class="absolute left-1 top-3">
+        <input type="text" placeholder="Email" class="focus:outline-none py-3 border-b-2 border-[#D3E4CD] pl-10 pr-5 text-[#bad8af] w-[100%] font-semibold" />
+      </div>
+
+      <div class="my-4 w-full relative">
+        <img src="./login/password-green.png" alt="" class="absolute left-1 top-3">
+        <input type="password" placeholder="Password" class="focus:outline-none py-3 border-b-2 border-[#D3E4CD] pl-10 pr-5 text-[#bad8af] w-[100%] font-semibold" />
+      </div>
+      <button class="px-14 py-3 text-lg my-5 bg-[#ADC2A9] font-semibold text-white rounded-4xl hover:bg-[#99a798] duration-200"> Sign in</button>
+      <p class="text-[#99A799] font-semibold text-lg my-3"> Don't have an account ?  <a href="/register" class="text-[#FFB97C]"> Register </a></p>
+    </div>
+
+    <!-- เอาไว้ใส่ภาพ -->
+    <div class="max-xl:hidden" data-aos="fade-up" data-aos-duration="1500">
+      <img src="./login/image.png" alt="">
+    </div>
+
+  </section>
+</main>
---
+++ frontend/src/routes/register/+page.svelte
@@ -0,0 +1,39 @@
+<main class="bg-[#FEF5ED] h-screen flex justify-center items-center">
+  <section class="grid grid-cols-2 w-[65vw] max-xl:w-full max-xl:grid-cols-1">
+
+    <!-- เอาไว้ใส่ภาพ -->
+    <div class="max-xl:hidden" data-aos="fade-right" data-aos-duration="1500">
+      <img src="./register/image.png" alt="">
+    </div>
+    <!-- register section -->
+    <div class="px-25 flex flex-col items-center justify-center max-lg:px-15 max-sm:px-5"  data-aos="fade-left" data-aos-duration="1500">
+      <h1 class="text-5xl font-semibold mb-5 text-center">Register</h1>
+
+      <div class="my-4 w-full relative">
+        <img src="./login/user-green.png" alt="" class="absolute left-1 top-3">
+        <input type="text" placeholder="Username" class="focus:outline-none py-3 border-b-2 border-[#D3E4CD] pl-10 pr-5 text-[#bad8af] w-[100%] font-semibold" />
+      </div>
+
+      <div class="my-4 w-full relative">
+        <img src="./login/email-green.png" alt="" class="absolute left-1 top-3">
+        <input type="text" placeholder="Email" class="focus:outline-none py-3 border-b-2 border-[#D3E4CD] pl-10 pr-5 text-[#bad8af] w-[100%] font-semibold" />
+      </div>
+
+      <div class="my-4 w-full relative">
+        <img src="./login/password-green.png" alt="" class="absolute left-1 top-3">
+        <input type="password" placeholder="Password" class="focus:outline-none py-3 border-b-2 border-[#D3E4CD] pl-10 pr-5 text-[#bad8af] w-[100%] font-semibold" />
+      </div>
+
+      <div class="my-4 w-full relative">
+        <img src="./login/password-green.png" alt="" class="absolute left-1 top-3">
+        <input type="password" placeholder="Confirm password" class="focus:outline-none py-3 border-b-2 border-[#D3E4CD] pl-10 pr-5 text-[#bad8af] w-[100%] font-semibold" />
+      </div>
+
+      <button class="px-14 py-3 text-lg my-5 bg-[#ADC2A9] font-semibold text-white rounded-4xl hover:bg-[#99a798] duration-200"> Sign up</button>
+      <p class="text-[#99A799] font-semibold text-lg my-3"> Already have an account ?  <a href="/" class="text-[#FFB97C]"> Login </a></p>
+    </div>
+
+    
+
+  </section>
+</main>
